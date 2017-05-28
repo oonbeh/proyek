32fa1f68f6f81bdf36f09819824bba311811c929
oonbeh <oonbeh@gmail.com>
2017-05-28 10:03:39 +0800

    Update Postingan1.js

--- Postingan1.js
+++ Postingan1.js
@@ -18,17 +18,10 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 {a_to_vh=true;}
 j++;}
 if(a_to_vh==false)
-{var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*4)+ 1);
-var xxx=null;
-if(x=="1"){xxx="how-to-read-forex-chart-properly.html"}
-if(x=="2"){xxx="google-drive-cloud.html"}
-if(x=="3"){xxx="ways-to-get-cheapest-health-insuranc.html"}
-if(x=="4"){xxx="stress-eating-helps.html"}a_to_ve[i].href="https://linkdownloadid.blogspot.com/2017/04/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+{var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href="https://safegoto.blogspot.com/2017/05/how-to-write-business-to-business.html?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
 {a_to_vk.innerHTML+=a_to_va;}}
 function a_to_fa()
 {var a_to_vf=new Array();protected_links=protected_links.replace(" ","");a_to_vf=protected_links.split(",");return a_to_vf;}
-auto_safelink();
